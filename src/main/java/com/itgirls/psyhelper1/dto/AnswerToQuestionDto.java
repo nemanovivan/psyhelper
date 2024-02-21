5da33135c8195ec79f3732376a3cae094dcd711a
mary <marianelalieben@gmail.com>
2024-02-21 18:53:28 +0300

    feature:21.02.2024 18:53 class AnswerToQuestionDto was created.

---
+++ src/main/java/com/itgirls/psyhelper1/dto/AnswerToQuestionDto.java
@@ -0,0 +1,23 @@
+package com.itgirls.psyhelper1.dto;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Getter
+@Setter
+public class AnswerToQuestionDto {
+    private UUID id;
+    private String answerText;
+    private boolean isRead;
+    private boolean isAuthorLiked;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private UUID userId;
+    private UUID questionId;
+}
