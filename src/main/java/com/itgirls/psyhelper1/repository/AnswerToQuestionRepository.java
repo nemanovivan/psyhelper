c1fff5d565030aa6a53e40806049ddf30afcd391
mary <marianelalieben@gmail.com>
2024-02-22 19:22:40 +0300

    feature:22.02.2024 19:22 class AnswerToQuestionRepository was created.

---
+++ src/main/java/com/itgirls/psyhelper1/repository/AnswerToQuestionRepository.java
@@ -0,0 +1,14 @@
+package com.itgirls.psyhelper1.repository;
+
+import com.itgirls.psyhelper1.model.AnswerToQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AnswerToQuestionRepository extends JpaRepository<AnswerToQuestion, UUID> {
+    Optional<AnswerToQuestion> findByUserId(UUID userId);
+    Optional<AnswerToQuestion> findByQuestionId(UUID questionId);
+}
