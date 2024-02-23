c2b0d6981f44bf2573d52ab30a32d26e39372ef8
mary <marianelalieben@gmail.com>
2024-02-23 23:16:48 +0300

    feature:23.02.2024 23:16 method deleteAnswerById was created.

--- src/main/java/com/itgirls/psyhelper1/service/AnswerToQuestionServiceImpl.java
+++ src/main/java/com/itgirls/psyhelper1/service/AnswerToQuestionServiceImpl.java
@@ -1,11 +1,30 @@
 package com.itgirls.psyhelper1.service;
 
+import com.itgirls.psyhelper1.model.AnswerToQuestion;
+import com.itgirls.psyhelper1.repository.AnswerToQuestionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class AnswerToQuestionServiceImpl {
+public class AnswerToQuestionServiceImpl implements AnswerToQuestionService{
+    private final AnswerToQuestionRepository answerRepository;
+
+    @Override
+    public void deleteAnswerById(UUID id) {
+        Optional<AnswerToQuestion> answer = answerRepository.findById(id);
+        if(answer.isPresent()) {
+            answerRepository.deleteById(id);
+            log.info("Answer with id {} " + id + " was deleted");
+        } else {
+            log.info("Can't find answer with id {} " + id);
+            throw new NoSuchElementException("No value present");
+        }
+    }
 }
