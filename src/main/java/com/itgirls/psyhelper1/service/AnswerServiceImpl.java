60b1054483adb26040e71dcbeea82bf6757f218c
mary <marianelalieben@gmail.com>
2024-02-25 15:27:46 +0300

    feature:25.02.2024 15:27 method createAnswer was created.

--- src/main/java/com/itgirls/psyhelper1/service/AnswerServiceImpl.java
+++ src/main/java/com/itgirls/psyhelper1/service/AnswerServiceImpl.java
@@ -1,8 +1,11 @@
 package com.itgirls.psyhelper1.service;
 
 import com.itgirls.psyhelper1.dto.AnswerDto;
+import com.itgirls.psyhelper1.mappers.AnswerMapper;
 import com.itgirls.psyhelper1.model.Answer;
+import com.itgirls.psyhelper1.model.Users;
 import com.itgirls.psyhelper1.repository.AnswerRepository;
+import com.itgirls.psyhelper1.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,8 +19,26 @@ import java.util.UUID;
 @Slf4j
 public class AnswerServiceImpl implements AnswerService {
     private final AnswerRepository answerRepository;
+    private final UsersRepository usersRepository;
+    private final AnswerMapper answerMapper;
 
-
+    @Override
+    public AnswerDto createAnswer(AnswerDto answerDto) {
+        log.info("Creating new answer: {}", answerDto);
+        //Получение пользователя по userId
+        Optional<Users> user = usersRepository.findById(answerDto.getUserId());
+        if (user.isEmpty()) {
+            throw new NoSuchElementException("User not found");
+        }
+        // Создать новый объект ответа и задать значения
+        Answer answer = answerMapper.toEntity(answerDto);
+        answer.setUserId(user.get());
+        answer.setRead(false);
+        answer.setAuthorLiked(false);
+        Answer savedAnswer = answerRepository.save(answer);
+        log.info("Answer created successfully id: {}", savedAnswer.getId());
+        return answerMapper.toDto(savedAnswer);
+    }
 
     @Override
     public void deleteAnswer(UUID id) {
@@ -25,9 +46,9 @@ public class AnswerServiceImpl implements AnswerService {
         Optional<Answer> optionalAnswer = answerRepository.findById(id);
         if (optionalAnswer.isPresent()) {
             answerRepository.deleteById(id);
-            log.info("Answer with id {} " + id + " was deleted");
+            log.info("Answer was deleted with id {}", id);
         } else {
-            log.info("Can't find answer with id {} " + id);
+            log.info("Can't find answer with id {} ", id);
             throw new NoSuchElementException("Answer not found.");
         }
     }
