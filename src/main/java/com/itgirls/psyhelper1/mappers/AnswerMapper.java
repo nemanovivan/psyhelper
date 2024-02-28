cbfadfd97b98475b6e87f26e334b4c17c01f2a06
mary <marianelalieben@gmail.com>
2024-02-28 16:56:22 +0300

    fix:28.02.2024 16:56 method toEntity was changed.

--- src/main/java/com/itgirls/psyhelper1/mappers/AnswerMapper.java
+++ src/main/java/com/itgirls/psyhelper1/mappers/AnswerMapper.java
@@ -1,6 +1,6 @@
 package com.itgirls.psyhelper1.mappers;
 
-import com.itgirls.psyhelper1.dto.AnswerCreateDto;
+//import com.itgirls.psyhelper1.dto.AnswerCreateDto;
 import com.itgirls.psyhelper1.dto.AnswerDto;
 import com.itgirls.psyhelper1.model.Answer;
 import org.mapstruct.Mapper;
@@ -9,7 +9,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface AnswerMapper {
     @Mapping(source = "userId", target = "userId.id")
-    Answer toEntity(AnswerCreateDto answerDto);
+    Answer toEntity(AnswerDto answerDto);
 
     @Mapping(source = "userId.id", target = "userId")
     AnswerDto toDto(Answer answer);
