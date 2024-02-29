203124e2fbdb1e0afe34e6656a2c6e69471dfe34
mary <marianelalieben@gmail.com>
2024-02-29 15:49:56 +0300

    feature:29.02.2024 15:49 two new methods map were added to class.

--- src/main/java/com/itgirls/psyhelper1/mappers/AnswerMapper.java
+++ src/main/java/com/itgirls/psyhelper1/mappers/AnswerMapper.java
@@ -3,8 +3,13 @@ package com.itgirls.psyhelper1.mappers;
 //import com.itgirls.psyhelper1.dto.AnswerCreateDto;
 import com.itgirls.psyhelper1.dto.AnswerDto;
 import com.itgirls.psyhelper1.model.Answer;
+import com.itgirls.psyhelper1.model.Users;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface AnswerMapper {
@@ -13,4 +18,14 @@ public interface AnswerMapper {
 
     @Mapping(source = "userId.id", target = "userId")
     AnswerDto toDto(Answer answer);
+
+    default Users map(UUID value) {
+        Users user = new Users();
+        user.setId(value);
+        return user;
+    }
+
+    default UUID map(Users value) {
+        return value.getId();
+    }
 }
