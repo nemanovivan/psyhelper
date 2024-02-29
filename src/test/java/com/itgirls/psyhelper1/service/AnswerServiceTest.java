a5f41daf8edcdf74d47aa39575b84107d6141f05
mary <marianelalieben@gmail.com>
2024-02-29 20:57:41 +0300

    feature:29.02.2024 20:57 method testCreateAnswer was created and method testDeleteAnswer was rewritten with a new logic.

--- src/test/java/com/itgirls/psyhelper1/service/AnswerServiceTest.java
+++ src/test/java/com/itgirls/psyhelper1/service/AnswerServiceTest.java
@@ -1,18 +1,19 @@
 package com.itgirls.psyhelper1.service;
 
+import com.itgirls.psyhelper1.dto.AnswerDto;
 import com.itgirls.psyhelper1.mappers.AnswerMapper;
 import com.itgirls.psyhelper1.model.Answer;
 import com.itgirls.psyhelper1.model.Users;
 import com.itgirls.psyhelper1.repository.AnswerRepository;
+import com.itgirls.psyhelper1.repository.UsersRepository;
 import com.itgirls.psyhelper1.service.impl.AnswerServiceImpl;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.ZonedDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,28 +24,64 @@ import static org.mockito.Mockito.when;
 public class AnswerServiceTest {
     @Mock
     AnswerRepository answerRepository;
+    @Mock
+    UsersRepository usersRepository;
 
     @Mock
     AnswerMapper answerMapper;
     @InjectMocks
     AnswerServiceImpl answerService;
 
-    UUID id = UUID.randomUUID();
-    UUID questionId = UUID.randomUUID();
-    ZonedDateTime createdAt = ZonedDateTime.now();
-    ZonedDateTime updatedAt = ZonedDateTime.now();
-    Users userId = new Users();
-    int rating = 5;
-    String answerText = "answerText";
-    private final Answer answer = new Answer(id, questionId, createdAt, updatedAt, userId, rating, answerText);
+    @Test
+    public void testCreateAnswer() {
+        // Создание тестовых данных
+        AnswerDto answerDto = new AnswerDto();
+        answerDto.setUserId(new Users());
+        answerDto.setAnswerText("text");
+
+        Users user = new Users();
+        user.setId(answerDto.getUserId().getId());
+
+        Answer answer = new Answer();
+        answer.setUserId(user);
+        answer.setAnswerText("answerText");
+        answer.setCreatedAt(answer.getCreatedAt());
+
+        Answer savedAnswer = new Answer();
+        savedAnswer.setId(UUID.randomUUID());
+
+        // Настройка мок объектов
+        when(usersRepository.findById(answerDto.getUserId().getId())).thenReturn(Optional.of(user));
+        when(answerRepository.save(Mockito.any(Answer.class))).thenReturn(savedAnswer);
+        when(answerMapper.toEntity(answerDto)).thenReturn(answer);
+
+        //Вызов метода
+        AnswerDto createdAnswer = answerService.createAnswer(answerDto);
+
+        //Проверка результата
+        Assertions.assertThat(createdAnswer).isNotNull();
+        Assertions.assertThat(createdAnswer.getUserId()).isEqualTo(answerDto.getUserId());
+        Assertions.assertThat(createdAnswer.getAnswerText()).isEqualTo(answerDto.getAnswerText());
+        Assertions.assertThat(createdAnswer.getId()).isEqualTo(savedAnswer.getId());
+
+        verify(usersRepository).findById(answerDto.getUserId().getId());
+        verify(answerRepository).save(answer);
+        verify(answerMapper).toDto(savedAnswer);
+    }
 
     @Test
-    public void testDeleteAnswerById() {
-        // Устанавка поведения для mock объектов
-        when(answerRepository.findById(id)).thenReturn(Optional.of(answer));
+    public void testDeleteAnswer() {
+        // Создание тестовых данных
+        UUID answerId = UUID.randomUUID();
+
+        // Настройка мок объекта
+        when(answerRepository.findById(answerId)).thenReturn(Optional.of(new Answer()));
+
         // Вызов метода
-        answerService.deleteAnswer(id);
-        // Проверка поведения
-        verify(answerRepository).deleteById(id);
+        answerService.deleteAnswer(answerId);
+
+        // Проверка результата
+        verify(answerRepository).findById(answerId);
+        verify(answerRepository).deleteById(answerId);
     }
 }
