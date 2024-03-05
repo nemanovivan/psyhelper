15ef2b6618abacc98da3248f5599a8798e111bce
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-03-05 00:40:48 -0800

    feature: add the new method findUsersByUsernameAndAssistantRating().

--- src/main/java/com/itgirls/psyhelper1/service/UsersService.java
+++ src/main/java/com/itgirls/psyhelper1/service/UsersService.java
@@ -4,14 +4,14 @@ import com.itgirls.psyhelper1.dto.UsersDto;
 import com.itgirls.psyhelper1.dto.UsersRegistrationDto;
 import com.itgirls.psyhelper1.model.Users;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface UsersService {
     void deleteUserById(UUID id);
     void save(Users user);
     UsersDto createUser(UsersRegistrationDto usersRegistrationDto);
-
     UsersDto findByUsername(String userName);
     UsersDto getInfoById(UUID id);
+    List<UsersDto> findUsersByUsernameAndAssistantRating(String username, int rating);
 }
