a62495222257eceffc5b05d8fbf6283619a64003
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-03-05 00:38:22 -0800

    fix: remove unnecessary methods.

--- src/main/java/com/itgirls/psyhelper1/service/AssistantService.java
+++ src/main/java/com/itgirls/psyhelper1/service/AssistantService.java
@@ -1,19 +1,17 @@
 package com.itgirls.psyhelper1.service;
 
 import com.itgirls.psyhelper1.dto.*;
+import com.itgirls.psyhelper1.dto.assistantDto.AssistantCreateDto;
+import com.itgirls.psyhelper1.dto.assistantDto.AssistantDto;
+import com.itgirls.psyhelper1.dto.assistantDto.AssistantUpdateDto;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface AssistantService {
     AssistantDto createAssistant(AssistantCreateDto assistantCreateDto);
 
-    AssistantUpdateResponseDto updateAssistant(AssistantUpdateDto assistantUpdateDto);
+    AssistantDto updateAssistant(AssistantUpdateDto assistantUpdateDto);
 
     void deleteAssistantById(UUID id);
-
-    List<AssistantSearchResponseDto> getAssistantsSortedByRatingDesc();
-
-    AssistantResponseDto getInfoById(UUID id);
-
-    AssistantResponseDto getAssistantByUsername(String username);
 }
