f522bd1c8acc5972311b857ea2a3d06192b4921e
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-03-05 00:06:19 -0800

    fix: remove all methods related to Users.

--- src/main/java/com/itgirls/psyhelper1/controller/AssistantController.java
+++ src/main/java/com/itgirls/psyhelper1/controller/AssistantController.java
@@ -1,11 +1,12 @@
 package com.itgirls.psyhelper1.controller;
 
-import com.itgirls.psyhelper1.dto.*;
+import com.itgirls.psyhelper1.dto.assistantDto.AssistantCreateDto;
+import com.itgirls.psyhelper1.dto.assistantDto.AssistantDto;
+import com.itgirls.psyhelper1.dto.assistantDto.AssistantUpdateDto;
 import com.itgirls.psyhelper1.service.AssistantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -15,32 +16,17 @@ public class AssistantController {
     private final AssistantService assistantService;
 
     @PostMapping("/create")
-    AssistantDto createUser(@RequestBody AssistantCreateDto assistantCreateDto) {
+    AssistantDto createAssistant(@RequestBody AssistantCreateDto assistantCreateDto) {
         return assistantService.createAssistant(assistantCreateDto);
     }
 
     @PostMapping("/update")
-    AssistantUpdateResponseDto updateAssistant(@RequestBody AssistantUpdateDto assistantUpdateDto) {
+    AssistantDto updateAssistant(@RequestBody AssistantUpdateDto assistantUpdateDto) {
         return assistantService.updateAssistant(assistantUpdateDto);
     }
 
     @DeleteMapping("/delete/{id}")
     void deleteAssistant(@PathVariable("id") UUID id) {
         assistantService.deleteAssistantById(id);
-    }
-
-    @GetMapping("/assistants")
-    List<AssistantSearchResponseDto> getAssistants() {
-        return assistantService.getAssistantsSortedByRatingDesc();
-    }
-
-    @GetMapping("/{id}")
-    AssistantResponseDto getInfoById(@PathVariable("id") UUID id) {
-        return assistantService.getInfoById(id);
-    }
-
-    @GetMapping("/{username}")
-    AssistantResponseDto getAssistantByUsername(@PathVariable("username") String username) {
-        return assistantService.getAssistantByUsername(username);
     }
 }
